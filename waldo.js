4ce2d7d12ea93a6cabb1f75d45a7d6521df4e902
Bo <bopeng48@gmail.com>
2017-04-06 18:38:46 +0000

    hi

--- waldo.js
+++ waldo.js
@@ -1,15 +1,19 @@
 // The second argument/parameter is expected to be a function
 
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
+
+  arr.forEach(found);
 }
 
-function actionWhenFound(index) {
+function actionWhenFound(currVal,index) {
+  if(currVal == "Waldo")
   console.log("Found him at "+index,"!");
+console.log(" done from within actionWHenFound");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
